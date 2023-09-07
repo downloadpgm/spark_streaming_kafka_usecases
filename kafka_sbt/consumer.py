78e615e348bba0273cb8b575f82e2131c8d9acf3
mkenjis <mkenjis@gmail.com>
2023-09-07 20:12:45 -0300

    Multiple fixes/changes

--- kafka_sbt/consumer.py
+++ kafka_sbt/consumer.py
@@ -2,7 +2,7 @@
 
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('test', bootstrap_servers='kfk1:9092,kfk2:9092,kfk3:9092')
+consumer = KafkaConsumer('result', bootstrap_servers='kfk1:9092,kfk2:9092,kfk3:9092')
 
 for message in consumer:
-    print (message)
+    print (message.value)
